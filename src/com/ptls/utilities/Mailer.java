280801723e9e6c461c01882ea0fbc39b7166e713
Rukhsana Ali <mirrukhsana1996@gmail.com>
2022-06-05 20:24:10 +0530

    removed passwoord

--- src/com/ptls/utilities/Mailer.java
+++ src/com/ptls/utilities/Mailer.java
@@ -10,7 +10,7 @@ public class Mailer {
 	public static void send(String to, String subject, String msg) {
 
 		final String user = "jkptlsteam@gmail.com";// change accordingly
-		final String pass = "password@ptls";
+		final String pass = "";
 
 		// 1st step) Get the session object
 		Properties props = new Properties();
@@ -23,7 +23,7 @@ public class Mailer {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(user, pass);
 			}
-		});
+		}); 
 		// 2nd step)compose message
 		try {
 			MimeMessage message = new MimeMessage(session);
